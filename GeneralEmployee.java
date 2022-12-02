d2dce6bbd3bb2b9dd3c61466ba4f37ac98b2b0b9
jaydrag7 <118785492+jaydrag7@users.noreply.github.com>
2022-12-01 19:09:32 -0500

    Delete GeneralEmployee.java

--- GeneralEmployee.java
+++
@@ -1,9 +0,0 @@
-public class GeneralEmployee extends Employee {
-
-    public GeneralEmployee(String name, String dob, Sex sex, String addr, String email, String telNum,EType type){
-        super(name,dob,sex,addr,email,telNum,type);
-
-    }
-
-    
-}
